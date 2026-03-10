66436406b2e179dabab2f2e5b1660f4caad0c099
Avishkar Renuse <avishkarrenuse06@gmail.com>
2026-03-10 13:22:28 +0530

    new code added

--- pg_scraper.py
+++ pg_scraper.py
@@ -1,44 +1,50 @@
 import requests
 import pandas as pd
+import os
 
 API_KEY = "AIzaSyAvSR7PVcIiC-Te4QM3m4CA03s6QCQQYlY"
 
 location = "Sangli"
 query = "PG hostel in " + location
 
-url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={API_KEY}"
+url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 
-response = requests.get(url)
+params = {
+    "query": query,
+    "key": API_KEY
+}
+
+file_path = "PG_List.xlsx"
+
+if os.path.exists(file_path):
+    os.remove(file_path)
+
+response = requests.get(url, params=params)
 data = response.json()
+
+print(data)   # debug output
 
 pg_data = []
 
-for i, place in enumerate(data['results'], start=1):
-    
+for i, place in enumerate(data.get('results', []), start=1):
+
     name = place.get('name')
     address = place.get('formatted_address')
     place_id = place.get('place_id')
-    
-    # Get phone number using details API
-    details_url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=formatted_phone_number&key={API_KEY}"
-    
-    details_response = requests.get(details_url)
-    details_data = details_response.json()
-    
-    phone = details_data.get("result", {}).get("formatted_phone_number", "Not Available")
-    
+
     maps_link = f"https://www.google.com/maps/place/?q=place_id:{place_id}"
-    
+
     pg_data.append({
         "Sr. no": i,
         "PG name": name,
-        "Contact No Pg": phone,
+        "Contact No Pg": "Not Available",
         "Located Area": address,
         "Link": maps_link
     })
 
 df = pd.DataFrame(pg_data)
 
-df.to_excel("PG_List.xlsx", index=False)
+# df.to_excel("output_PG_List.xlsx", index=False)
+df.to_excel(file_path, index=False)
 
-print("Data saved to PG_List.xlsx")
+print("Data saved successfully")
